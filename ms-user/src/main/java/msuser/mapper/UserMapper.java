8a5b428bc2c10d33c8f420dcd55b72632de94fe9
NihadVeliyev <nihadv795@gmail.com>
2025-09-19 02:47:07 +0400

    Refactor UserMapper to define mapping methods for UserReq and UserRes

--- ms-user/src/main/java/msuser/mapper/UserMapper.java
+++ ms-user/src/main/java/msuser/mapper/UserMapper.java
@@ -1,8 +1,17 @@
 package msuser.mapper;
 
+import msuser.dto.UserReq;
+import msuser.dto.UserRes;
+import msuser.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
-public class UserMapper {
+public interface UserMapper {
+      @Mapping(target = "id",ignore = true)
+      @Mapping(target = "updatedAt",ignore = true)
+      @Mapping(target = "createdAt",ignore = true)
+      User toEntity(UserReq userReq);
+      UserRes toResponse(User user);
 
 }
