bf6279b49bfaeb1f643a44e95e0d20ef6d867a87
NihadVeliyev <nihadv795@gmail.com>
2025-09-19 02:50:04 +0400

    Add UserService class for user-related operations

---
+++ ms-user/src/main/java/msuser/service/UserService.java
@@ -0,0 +1,12 @@
+package msuser.service;
+
+import lombok.RequiredArgsConstructor;
+import msuser.mapper.UserMapper;
+import msuser.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+}
