ce2329f6fa1d970c7bae8475c5ae27c82ed6683f
NihadVeliyev <nihadv795@gmail.com>
2025-09-19 02:16:49 +0400

    Add fields to User entity for ID, username, password, and timestamps

--- ms-user/src/main/java/msuser/model/User.java
+++ ms-user/src/main/java/msuser/model/User.java
@@ -1,10 +1,28 @@
 package msuser.model;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
 @Entity
 @Data
 @Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(unique = true,nullable = false)
+    private String password;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
 
 
 
