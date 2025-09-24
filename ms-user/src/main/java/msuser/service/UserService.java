dc4ade20ce7c578b00dd3c6fdad10a6b10766f60
NihadVeliyev <nihadv795@gmail.com>
2025-09-24 17:18:17 +0400

    Refactor UserService to remove TODO comments and clean up method formatting

--- ms-user/src/main/java/msuser/service/UserService.java
+++ ms-user/src/main/java/msuser/service/UserService.java
@@ -8,20 +8,16 @@ import msuser.model.User;
 import msuser.repository.UserRepository;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserMapper userMapper;
     private final UserRepository userRepository;
-    //TODO createUser deleteUser getUserById
-
     public  UserRes createUser(UserReq userReq){
         User user=userMapper.toEntity(userReq);
         userRepository.save(user);
         UserRes userRes=userMapper.toResponse(user);
         return userRes;
-
     }
     public UserRes getUserById(Long id){
         return userMapper.toResponse(userRepository.findById(id)
