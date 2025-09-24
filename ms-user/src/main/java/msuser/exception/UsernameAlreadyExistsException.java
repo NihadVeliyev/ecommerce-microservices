075f5cefdf24ca30d624c9a21d5e71bee5f3ce73
NihadVeliyev <nihadv795@gmail.com>
2025-09-24 17:34:45 +0400

    Add UsernameAlreadyExistsException class for handling username conflicts

---
+++ ms-user/src/main/java/msuser/exception/UsernameAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package msuser.exception;
+
+public class UsernameAlreadyExistsException extends RuntimeException {
+    public UsernameAlreadyExistsException(String message) {
+        super(message);
+    }
+}
