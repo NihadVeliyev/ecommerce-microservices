648317a4c5faa5d3bc25d378dbda070e95a10191
NihadVeliyev <nihadv795@gmail.com>
2025-09-24 16:50:22 +0400

    Implement createUser method in UserService for user creation

--- ms-user/src/main/java/msuser/service/UserService.java
+++ ms-user/src/main/java/msuser/service/UserService.java
@@ -1,7 +1,10 @@
 package msuser.service;
 
 import lombok.RequiredArgsConstructor;
+import msuser.dto.UserReq;
+import msuser.dto.UserRes;
 import msuser.mapper.UserMapper;
+import msuser.model.User;
 import msuser.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +13,20 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserMapper userMapper;
     private final UserRepository userRepository;
+    //TODO createUser deleteUser getUserById
+
+    public  UserRes createUser(UserReq userReq){
+        User user=userMapper.toEntity(userReq);
+        userRepository.save(user);
+        UserRes userRes=userMapper.toResponse(user);
+        return userRes;
+
+    }
+
+
+
+
+
 
 
 }
