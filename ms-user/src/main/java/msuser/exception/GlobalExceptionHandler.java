25b55015ce40d6bab98ce5b316b5111e1ce53359
NihadVeliyev <nihadv795@gmail.com>
2025-09-25 22:02:18 +0400

    Add exception handler for UsernameAlreadyExistsException in GlobalExceptionHandler

--- ms-user/src/main/java/msuser/exception/GlobalExceptionHandler.java
+++ ms-user/src/main/java/msuser/exception/GlobalExceptionHandler.java
@@ -1,16 +1,17 @@
 package msuser.exception;
-
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(UsernameAlreadyExistsException.class)
     public ResponseEntity<String> handleDuplicateUsername(UsernameAlreadyExistsException exception){
         return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
-
     }
+  
+
+
+
+
 }
