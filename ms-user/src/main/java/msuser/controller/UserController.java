61bb5961314590b9ba74757c5e3f7c7eeeab5078
NihadVeliyev <nihadv795@gmail.com>
2025-09-29 16:49:22 +0400

    Remove unused HttpStatusCode import from UserController

--- ms-user/src/main/java/msuser/controller/UserController.java
+++ ms-user/src/main/java/msuser/controller/UserController.java
@@ -5,7 +5,6 @@ import msuser.dto.UserReq;
 import msuser.dto.UserRes;
 import msuser.service.UserService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
