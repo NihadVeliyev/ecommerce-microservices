bd01d0966bc99c7d52086252a1489c5b228c88c2
NihadVeliyev <nihadv795@gmail.com>
2025-09-29 15:44:00 +0400

    Add createUser method to UserController for user registration

--- ms-user/src/main/java/msuser/controller/UserController.java
+++ ms-user/src/main/java/msuser/controller/UserController.java
@@ -1,6 +1,13 @@
 package msuser.controller;
 
 import lombok.RequiredArgsConstructor;
+import msuser.dto.UserReq;
+import msuser.dto.UserRes;
+import msuser.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
+     private final UserService userService;
+     @PostMapping
+     public ResponseEntity<UserRes> createUser(UserReq userReq){
+          UserRes userRes1=userService.createUser(userReq);
+          return  ResponseEntity.status(HttpStatus.CREATED).body(userRes1);
+     }
 
 }
