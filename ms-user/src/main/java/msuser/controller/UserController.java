fb4b60c31e9e272598a035a8ccade68484d7ce96
NihadVeliyev <nihadv795@gmail.com>
2025-09-29 16:53:59 +0400

    Add validation annotations to createUser and getUserById methods in UserController

--- ms-user/src/main/java/msuser/controller/UserController.java
+++ ms-user/src/main/java/msuser/controller/UserController.java
@@ -1,5 +1,6 @@
 package msuser.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import msuser.dto.UserReq;
 import msuser.dto.UserRes;
@@ -17,12 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
      private final UserService userService;
      @PostMapping
-     public ResponseEntity<UserRes> createUser(UserReq userReq){
+     public ResponseEntity<UserRes> createUser(@Valid UserReq userReq){
           UserRes userRes1=userService.createUser(userReq);
           return  ResponseEntity.status(HttpStatus.CREATED).body(userRes1);
      }
      @GetMapping("/{id}")
-     public ResponseEntity<UserRes> getUserById(Long id){
+     public ResponseEntity<UserRes> getUserById(@Valid Long id){
           return ResponseEntity.status(HttpStatus.OK).body(userService.getUserById(id));
      }
 
